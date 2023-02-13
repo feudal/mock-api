c2f08f17f13cb09cd53d6ecacd86dc8a960913bf
Dmitri Calmîș <calmis87@gmail.com>
2023-02-13 11:16:19 +0200

    add api for get by id

--- pages/api/mock-api/[id].ts
+++ pages/api/mock-api/[id].ts
@@ -4,7 +4,23 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "utils";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  if (req.method === "DELETE") {
+  if (req.method === "GET") {
+    if (!req.query.id) {
+      res.status(400).json({ message: "Missing id" });
+      return;
+    }
+
+    await db.connect();
+    const mockApi = await MockApi.findById(req.query.id);
+    await mockApi?.populate("fields");
+    await db.disconnect();
+
+    if (mockApi) {
+      res.status(200).json(mockApi);
+    } else {
+      res.status(404).json({ error: "Not found" });
+    }
+  } else if (req.method === "DELETE") {
     if (!req.query.id) {
       res.status(400).json({ message: "Missing id" });
       return;
